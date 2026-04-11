b524bd0198955387e25b97d83b1a474e74489d51
ORION <keneth@kaliKeneth.Keneth>
2026-04-11 12:15:51 +0200

    Interface modifié et recherche

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var express = require('express');
+
+app.use(express.static(__dirname + '/images'));
+
+//Fonction trouver adresse IP locale
+const os = require('os');
+
+function getLocalIP() {
+    const interfaces = os.networkInterfaces();
+    for (const interfaceName of Object.keys(interfaces)) {
+        for (const iface of interfaces[interfaceName]) {
+            // Filtre pour IPv4, non interne (exclut 127.0.0.1)
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '127.0.0.1'; // Retourne localhost si aucune IP trouvée
+}
+
+        //console.log('Adresse IP locale :', getLocalIP());   
+//Fin fonction trouver adresse IP locale
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/index.html')
@@ -15,6 +37,8 @@ io.on('connection', function(socket){
         io.emit('messages', message);//Renvoie du message au client
     })//écoute des messages
 })//socket.io écoute les connexions et déconnexions et affiche un message
-http.listen(3000, function(){
+
+http.listen(3000, '0.0.0.0', function(){
     console.log("Le serveur écoute le port 3000")
+    console.log("Accès distant à l'adresse: http://" + getLocalIP() + ":3000")
 })//écoute du port 3000 avec message de confirmation dans la console
